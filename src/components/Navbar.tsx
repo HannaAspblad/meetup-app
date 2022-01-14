18165b7f82f82516bd3f828f03c6a37fe2c0cbb8
hanna <hanna.aspblad@iths.se>
2022-01-14 12:15:28 +0100

    fixed menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,21 +4,42 @@ import * as API from "../api/api"
 
 const Navbar = () => {
   const [user, setUser] = useState(sessionStorage.getItem("User"))
+  const [displayMenu, setDisplayMenu] = useState(false)
 
   useEffect(() => {
     try {
       getUser()
     } catch (err) {}
   })
-
+  const toggleMenu = () => {
+    if (!displayMenu) {
+      setUser(sessionStorage.getItem("User"))
+      setDisplayMenu(true)
+    } else {
+      setDisplayMenu(false)
+    }
+  }
   const getUser = async () => {
     const currentUser = await API.getUser(user)
-    return currentUser
+
+    setUser(currentUser.id)
   }
   return (
     <div className="navbar-wrapper">
-      <Link to="/">All meetups</Link>
-      <Link to={`/user/${user}`}>Your page</Link>
+      <button onClick={toggleMenu}>Menu</button>
+      {displayMenu ? (
+        <div className="navbar-menu">
+          <Link to="/" onClick={toggleMenu}>
+            All meetups
+          </Link>
+
+          <Link to={`/user/${user}`} onClick={toggleMenu}>
+            Your page
+          </Link>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   )
 }
